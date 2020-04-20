ea53ceaa898ca3385f9e63d3a0c6ab5104da9c09
vuhe <zhuhe202@qq.com>
2020-04-20 16:07:52 +0800

    init: 初始化后端框架

---
+++ backend/src/test/java/site/zhuhe/weibo/WeiboApplicationTests.java
@@ -0,0 +1,13 @@
+package site.zhuhe.weibo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeiboApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
