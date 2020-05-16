7fe29c4bd0581ed476d5dbb8ae8eb3adfe9e1a88
vuhe <zhuhe202@qq.com>
2020-05-16 20:55:01 +0800

    feat: 添加测试内容

--- backend/src/test/java/site/zhuhe/weibo/WeiboApplicationTests.java
+++ backend/src/test/java/site/zhuhe/weibo/WeiboApplicationTests.java
@@ -1,14 +1,45 @@
 package site.zhuhe.weibo;
 
 import org.apache.shiro.crypto.hash.Sha256Hash;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 @SpringBootTest
+@RunWith(SpringRunner.class)
+@WebAppConfiguration
 class WeiboApplicationTests {
 
+    @Autowired
+    private WebApplicationContext webContext;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void setup() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webContext).build();
+    }
+
     @Test
-    void contextLoads() {
+    void contextLoads() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                .get("/get/1")
+                // 设置返回值类型为utf-8，否则默认为ISO-8859-1
+                .accept(MediaType.APPLICATION_JSON_UTF8_VALUE)
+                .param("name", "Tom"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(MockMvcResultHandlers.print());
     }
 
     @Test
